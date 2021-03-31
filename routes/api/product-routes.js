32bbd7f7df719cbdab2df44b8479d51f1fb3e60c
damian.deleon <damianmdeleon@gmail.com>
2021-03-30 22:59:06 -0500

    corrected api routes for tag-routes as well as index.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,19 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll({ include: [{ model: Category }, {model: Tag}]}
+    const productData = await Product.findAll(
+      {
+         include: [
+           { 
+             model: Category, 
+             attributes: ['category_name']
+            }, 
+            { 
+              model: Tag,
+              attributes: ['tag_name']
+            }
+        ] 
+      }
     );
     res.status(200).json(productData);
   } catch (err) {
@@ -23,7 +35,7 @@ router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
       // JOIN with tags through tag-routes
-      include: [{ model: Tag}, {model: Category }]
+      include: [{ model: Tag }, { model: Category }]
     });
 
     if (!productData) {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagData = await Product.findAll(
+    const tagData = await Tag.findAll(
       { include: [{ model: Product}]}
     );
     res.status(200).json(tagData);
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Category.findByPk(req.params.id, {
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product}],
     });
 
